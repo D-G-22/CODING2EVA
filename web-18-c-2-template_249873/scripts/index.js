489bce8bbdac3c03db9653b07a502fb0942ef16c
D-G-22 <gramtheke2020@gmail.com>
2022-05-30 16:26:24 +0530

    add

---
+++ web-18-c-2-template_249873/scripts/index.js
@@ -0,0 +1,33 @@
+// Add the coffee to local storage with key "coffee"
+
+const url="https://masai-mock-api.herokuapp.com/coffee/menu";
+
+
+
+async function getData()
+{
+    try{
+        let res= await fetch(url);
+        let users= await.res.json();
+        append(users.data);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+getData();
+
+function append(data)
+{
+    let container=document.getElementById("menu");
+    data.foreach(function(el){
+        let img=document.createElement("img");
+        img.src=el.avatar;
+
+        let div=document.createElement("div");
+        div.append(img)
+        container.append(div)
+    })
+}
+
+
